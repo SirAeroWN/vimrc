924c929e0ee51e15600278247f25db629d6a0cbf
norv <sicklyserver@gmail.com>
2018-10-16 16:06:07 -0400

    added smarter parsing to make adding plugins easier

--- update_plugins.py
+++ update_plugins.py
@@ -4,63 +4,66 @@ import tempfile
 import requests
 
 from os import path
+from collections import namedtuple
 
 
-#--- Globals ----------------------------------------------
-PLUGINS = """
-ale https://github.com/w0rp/ale
-vim-yankstack https://github.com/maxbrunsfeld/vim-yankstack
-ack.vim https://github.com/mileszs/ack.vim
-bufexplorer https://github.com/corntrace/bufexplorer
-ctrlp.vim https://github.com/ctrlpvim/ctrlp.vim
-mayansmoke https://github.com/vim-scripts/mayansmoke
-nerdtree https://github.com/scrooloose/nerdtree
-nginx.vim https://github.com/chr4/nginx.vim
-open_file_under_cursor.vim https://github.com/amix/open_file_under_cursor.vim
-snipmate-snippets https://github.com/scrooloose/snipmate-snippets
-tlib https://github.com/vim-scripts/tlib
-vim-addon-mw-utils https://github.com/MarcWeber/vim-addon-mw-utils
-vim-bundle-mako https://github.com/sophacles/vim-bundle-mako
-vim-coffee-script https://github.com/kchmck/vim-coffee-script
-vim-colors-solarized https://github.com/altercation/vim-colors-solarized
-vim-indent-object https://github.com/michaeljsmith/vim-indent-object
-vim-less https://github.com/groenewege/vim-less
-vim-markdown https://github.com/tpope/vim-markdown
-vim-pyte https://github.com/therubymug/vim-pyte
-vim-snipmate https://github.com/garbas/vim-snipmate
-vim-snippets https://github.com/honza/vim-snippets
-vim-surround https://github.com/tpope/vim-surround
-vim-expand-region https://github.com/terryma/vim-expand-region
-vim-multiple-cursors https://github.com/terryma/vim-multiple-cursors
-vim-fugitive https://github.com/tpope/vim-fugitive
-goyo.vim https://github.com/junegunn/goyo.vim
-vim-zenroom2 https://github.com/amix/vim-zenroom2
-vim-repeat https://github.com/tpope/vim-repeat
-vim-commentary https://github.com/tpope/vim-commentary
-vim-go https://github.com/fatih/vim-go
-vim-gitgutter https://github.com/airblade/vim-gitgutter
-gruvbox https://github.com/morhetz/gruvbox
-vim-flake8 https://github.com/nvie/vim-flake8
-vim-pug https://github.com/digitaltoad/vim-pug
-lightline.vim https://github.com/itchyny/lightline.vim
-lightline-ale https://github.com/maximbaz/lightline-ale
-vim-abolish https://github.com/tpope/tpope-vim-abolish
-mru.vim https://github.com/vim-scripts/mru.vim
-vim-markdown https://github.com/plasticboy/vim-markdown
-comfortable-motion.vim https://github.com/yuttie/comfortable-motion.vim
-vim-sublime-monokai https://github.com/ErichDonGubler/vim-sublime-monokai
-dracula-theme https://github.com/dracula/dracula-theme
-palenight.vim https://github.com/drewtempelmeyer/palenight.vim
-onedark.vim https://github.com/joshdick/onedark.vim
-nord-vim https://github.com/arcticicestudio/nord-vim
-ayu-vim https://github.com/ayu-theme/ayu-vim
-vim-startify https://github.com/mhinz/vim-startify
-carbonized https://github.com/nightsense/carbonized
-AfterColors.vim https://github.com/vim-scripts/AfterColors.vim
-vim-easymotion https://github.com/easymotion/vim-easymotion
+# --- Globals ----------------------------------------------
+Repo = namedtuple('Repo', ['path', 'name'])
+
+PLUGINS="""
+w0rp/ale
+maxbrunsfeld/vim-yankstack
+mileszs/ack.vim
+corntrace/bufexplorer
+ctrlpvim/ctrlp.vim
+scrooloose/nerdtree
+amix/open_file_under_cursor.vim
+vim-scripts/tlib
+MarcWeber/vim-addon-mw-utils
+michaeljsmith/vim-indent-object
+tpope/vim-fugitive
+junegunn/goyo.vim
+amix/vim-zenroom2
+tpope/vim-repeat
+tpope/vim-commentary
+airblade/vim-gitgutter
+digitaltoad/vim-pug
+itchyny/lightline.vim
+maximbaz/lightline-ale
+tpope/vim-abolish
+vim-scripts/mru.vim
+mhinz/vim-startify
+vim-scripts/AfterColors.vim
+
+" autocomplete
+
+" motions
+easymotion/vim-easymotion
+yuttie/comfortable-motion.vim
+
+" repl
+jalvesaq/vimcmdline
+
+" language specific
+fatih/vim-go
+nvie/vim-flake8
+tpope/vim-markdown
+
+" colorschemes
+nightsense/carbonized
+ErichDonGubler/vim-sublime-monokai
+dracula/dracula-theme
+drewtempelmeyer/palenight.vim
+joshdick/onedark.vim
+arcticicestudio/nord-vim
+ayu-theme/ayu-vim
+morhetz/gruvbox
+altercation/vim-colors-solarized
+vim-scripts/peaksea
+twerth/ir_black
 """.strip()
 
-GITHUB_ZIP = '%s/archive/master.zip'
+GITHUB_ZIP = 'https://github.com/%s/archive/master.zip'
 
 SOURCE_DIR = path.join(path.dirname(__file__), 'sources_non_forked')
 
@@ -90,15 +93,27 @@ def download_extract_replace(plugin_name, zip_path, temp_dir, source_dir):
 
 
 def update(plugin):
-    name, github_url = plugin.split(' ')
-    zip_path = GITHUB_ZIP % github_url
-    download_extract_replace(name, zip_path, temp_directory, SOURCE_DIR)
+    zip_path = GITHUB_ZIP % plugin.path
+    download_extract_replace(plugin.name, zip_path, temp_directory, SOURCE_DIR)
+
+
+def parse_plugins(full_str):
+    qualified_names = list(filter(lambda s: s != '' and not s.startswith('"'), [s.strip() for s in full_str.splitlines()]))
+    global Repo
+    return [Repo(path=s, name=s.split('/')[-1]) for s in qualified_names]
 
 
 if __name__ == '__main__':
     temp_directory = tempfile.mkdtemp()
     print(temp_directory)
 
+    try:
+        plugins = parse_plugins(PLUGINS)
+        [update(x) for x in plugins]
+    finally:
+        shutil.rmtree(temp_directory)
+
+    quit()
     try:
         plugins = PLUGINS.splitlines()
         plugins.sort()
